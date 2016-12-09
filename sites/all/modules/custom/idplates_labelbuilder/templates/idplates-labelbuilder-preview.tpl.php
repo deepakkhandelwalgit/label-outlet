418e007daece8c4119d4b335ae71d1c77563f935
Jace Bennest <jbennest@acromediainc.com>
2016-12-09 15:33:41 -0800

    Task #240745: load labelbuilder with nid

--- sites/all/modules/custom/idplates_labelbuilder/templates/idplates-labelbuilder-preview.tpl.php
+++ sites/all/modules/custom/idplates_labelbuilder/templates/idplates-labelbuilder-preview.tpl.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file This template will show the user the label preview.
  *
@@ -6,10 +7,18 @@
  * - $label: the label to preview.
  */
 
+$layout_wrapper = entity_metadata_wrapper('taxonomy_term', $label['layout']);
+$image = $layout_wrapper->field_ll_image->value();
 ?>
 <div class="idplates-labelbuilder-preview">
-  <?php print $label->name; ?>
+  <?php print render(theme('image', array(
+    'path' => $image['uri'],
+    'attributes' => array(),
+  ))); ?>
   <?php print $label['idplates_preview']['color']; ?>
   <?php print $label['idplates_preview']['title']; ?>
   <?php print $label['idplates_preview']['organization']; ?>
+  <?php print $label['idplates_preview']['extra']; ?>
+  <?php print $label['idplates_preview']['numbering_options']; ?>
+  <?php print $label['idplates_preview']['specs']; ?>
 </div>
