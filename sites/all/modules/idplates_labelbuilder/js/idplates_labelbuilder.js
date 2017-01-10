37c5a4c9fdb81235c3f36c3bf406862ab6211cdd
Jace Bennest <jbennest@acromediainc.com>
2017-01-10 15:52:28 -0800

    Task #242094: try the custom ajax trigger

--- sites/all/modules/idplates_labelbuilder/js/idplates_labelbuilder.js
+++ sites/all/modules/idplates_labelbuilder/js/idplates_labelbuilder.js
@@ -1,7 +1,17 @@
 (function ($) {
   Drupal.behaviors.idplatesLabelBuilder = {
     attach: function (context, settings) {
+      $("#edit-qty").on('pause:typing', setTimeout(function () {
+            var e = jQuery.Event('keydown', {which: 13});// # Enter Key
+            $(this).trigger(e);
+          }, 1000)
+      );
+
+      $("#edit-qty").on('keyup', function (e) {
+        $(this).trigger('pause:typing');
+      });
+
     }
-  }
+  };
 })
 (jQuery);
