d7ce107dfd33a12aa486b5d0516084871ad8e2a7
Jace Bennest <jbennest@acromediainc.com>
2017-01-24 15:58:25 -0800

    Task #240745: consecutive numebering hooked up

--- sites/all/modules/idplates_labelbuilder/js/idplates_labelbuilder.js
+++ sites/all/modules/idplates_labelbuilder/js/idplates_labelbuilder.js
@@ -4,13 +4,29 @@
 
       var timer;
 
-      $(".idplates-labelbuilder-qty-price-wrapper #edit-qty").on('keyup', function (e) {
-        var $this = $(this);
+      $('.idplates-labelbuilder-qty-price-wrapper #edit-qty').on('keyup', function (e) {
         clearTimeout(timer);
         timer = setTimeout(function () {
-          $this.blur();
-          $this.focus();
-        }, 600);
+          $('#' + e.target.id).blur();
+          // $(document).ajaxComplete(function (event, xhr, settings) {
+          //   $('.idplates-labelbuilder-qty-price-wrapper #edit-qty').focus();
+          // });
+        }, 800);
+      });
+
+      $('#idplates-labelbuilder-customize-form [id^="edit-text"]').on('keyup', function (e) {
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+          $('#' + e.target.id).blur();
+          // $(document).ajaxComplete(function (event, xhr, settings) {
+          //   console.log($(event));
+          //   console.log(event);
+          //   console.log(e.target.id);
+          //   if (event.target.activeElement.id == e.target.id) {
+          //     $('#' + e.target.id).focus();
+          //   }
+          // });
+        }, 800);
       });
 
     }
