83db7c33b4d9f6fdd8850867cb19a2774c703098
Austin Elling <aelling@gmail.com>
2017-02-15 15:36:18 +0000

    update

--- sites/all/themes/retailplus/views-view-list--slideshow_boxed--block.tpl.php
+++ sites/all/themes/retailplus/views-view-list--slideshow_boxed--block.tpl.php
@@ -51,34 +51,3 @@
         </div>
     </div>
 </div>
-
-
-drupal_add_js('
-
-    var tpj=jQuery;
-    tpj.noConflict();
-    
-    tpj(document).ready(function($) { 
-
-    if (tpj.fn.cssOriginal!=undefined)
-        tpj.fn.css = tpj.fn.cssOriginal;
-
-    
-    var api = tpj(".bannercontainer .banner").revolution({
-        delay:"'.$rs_boxed_effect_time.'",
-        startheight: 450,
-        startwidth: 1140,
-        onHoverStop: "off",
-        navigationStyle: "'.$rs_directional_preview.'"
-    });
-    
-    api.bind("revolution.slide.onloaded",function (e,data) {
-         jQuery(".tparrows.default").css("display", "block");
-         jQuery(".tp-bullets").css("display", "block");
-         jQuery(".tp-bannertimer").css("display", "block");
-    });
-
-});',
-array('type' => 'inline', 'scope' => 'header'));
-	
-?>
