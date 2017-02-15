0b8cecd65d148ec7e4021c753b0f2604ab7bb16c
Austin Elling <aelling@gmail.com>
2017-02-15 15:48:02 +0000

    update

--- sites/all/themes/retailplus/views-view-list--slideshow_boxed--block.tpl.php
+++ sites/all/themes/retailplus/views-view-list--slideshow_boxed--block.tpl.php
@@ -1,6 +1,6 @@
 <div class="container boxed-slideshow">
     <div id="slideshow" class="bannercontainer main-slider">
-
+        <?php $rs_boxed_effect=theme_get_setting('rs_slideshow_boxed_effect'); ?>
         <div class="banner">
             <ul>
             <?php foreach ($rows as $id => $row) { ?>
@@ -18,14 +18,20 @@
                 if ($node->type=='mt_slideshow_entry') {  ?>
                     <?php if ($node->field_mt_slideshow_entry_path) { ?>
                     <?php $path = $node->field_mt_slideshow_entry_path[$lang][0]['value']; ?>
-                    <li data-link="<?php print url($path); ?>">
+                    <li data-link="<?php print url($path); ?>" data-transition="<?php print $rs_boxed_effect ?>" data-masterspeed="800">
                     <?php } else { ?>
-                    <li >
+                    <li data-transition="<?php print $rs_boxed_effect ?>" data-masterspeed="800">
                     <?php } ?>
 
                         <img src="<?php print $image; ?>" title="<?php print $title; ?>" alt="<?php print $alt; ?>"/>
 
-                        <div>
+                        <div class="tp-caption sft fadeout"
+                        data-x="left"
+                        data-y="top"
+                        data-speed="500"
+                        data-start="1200"
+                        data-voffset="50"
+                        data-easing="Power0.easeIn">
                             <div class="title">
                                 <?php $title = $node->title; ?>
                                 <?php if ($node->field_mt_slideshow_entry_path) { ?>
@@ -40,7 +46,7 @@
                         </div>
                     </li>
                 <?php } else { ?>
-                    <li data-link="<?php print $nodeurl ?>" >
+                    <li data-transition="<?php print $rs_boxed_effect ?>" data-link="<?php print $nodeurl ?>" data-masterspeed="800">
                         <?php print $row; ?>
                     </li>
                 <?php } ?> 
@@ -51,3 +57,42 @@
         </div>
     </div>
 </div>
+<?php
+$rs_boxed_effect_time = (int) theme_get_setting('rs_slideshow_boxed_effect_time')*1000;
+
+if (theme_get_setting('rs_slideshow_boxed_preview')) {
+    $rs_directional_preview = "preview1";
+} else {
+    $rs_directional_preview = "square";
+}
+
+
+drupal_add_js('
+
+    var tpj=jQuery;
+    tpj.noConflict();
+    
+    tpj(document).ready(function($) { 
+
+    if (tpj.fn.cssOriginal!=undefined)
+        tpj.fn.css = tpj.fn.cssOriginal;
+
+    
+    var api = tpj(".bannercontainer .banner").revolution({
+        delay:"'.$rs_boxed_effect_time.'",
+        startheight: 450,
+        startwidth: 1140,
+        onHoverStop: "off",
+        navigationStyle: "'.$rs_directional_preview.'"
+    });
+    
+    api.bind("revolution.slide.onloaded",function (e,data) {
+         jQuery(".tparrows.default").css("display", "block");
+         jQuery(".tp-bullets").css("display", "block");
+         jQuery(".tp-bannertimer").css("display", "block");
+    });
+
+});',
+array('type' => 'inline', 'scope' => 'header'));
+	
+?>
